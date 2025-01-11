2d34717f8582980d639402adb982f9a6856d77d3
danzapata <142749695+danzapata@users.noreply.github.com>
2025-01-10 22:01:27 -0500

    beta de solicitudes

--- Practica1/sistema.py
+++ Practica1/sistema.py
@@ -58,7 +58,8 @@ class Sistema:
             print("3. Eliminar usuarios.")
             print("4. cambiar contraseñas.")
             print("5. Generar inventario en txt.")
-            print("6. Salir.")
+            print("6. Atender solicitudes.")
+            print("7. Salir.")
             indice = int(input("Ingrese un indice: "))
 
             if indice == 1: # Consulta equipos -----------------------------------------------------------------------
@@ -258,10 +259,81 @@ class Sistema:
                     print("Cedula incorrecta, vuelva a intentarlo")
 
             elif indice == 6:
-                break
-            else:
-                print("Indice no valido")
+                # Atender solicitudes
+                # leer solicitudes en Control de cambio
+                control = open("Practica1/archivosOp/solicitudes.txt", "r+") 
+                texto = control.read().split("\n")
+                print("Imprimimos todas las solicitudes")
 
+                contador = 1
+                for i in texto:
+                    print(f"{contador}. {i}")
+                    contador+=1
+
+                indice = int(input("Selecione la solicitud que desea revisar: "))
+                indice = indice-1
+                comparador = texto[indice].split(" ")
+                if "pendiente" not in comparador:
+                    print("La solicitud ya fue contestada.")
+                else: 
+                    respuesta = int(input("Desea 1. rechazar o 2. aceptar, indique el número de su respuesta: "))
+                    if respuesta == 1:
+                        nuevo = texto[indice].split(" ")
+                        nuevo[-1] = "rechazar"
+                        print("operación realizada con éxito")
+                        concatenar = ""
+                        for i in nuevo:
+                            if i is not nuevo[-1]:
+                                concatenar+=i+" "
+                            else:
+                                concatenar+=i
+                        texto[indice] = concatenar
+                        j = open("Practica1/archivosOp/solicitudes.txt", "w")
+                        for i in texto:
+                            if i is not texto[-1]:
+                                j.write(f"{i}\n")
+                            else:
+                                j.write(f"{i}")
+                        j.close()
+                    elif respuesta == 2:
+                        nuevo = texto[indice].split(" ")
+                        nuevo[-1] = "aceptar"
+                        print("operación realizada con éxito, inventario general actualizado.")
+                        concatenar = ""
+                        for i in nuevo:
+                            if i is not nuevo[-1]:
+                                concatenar+=i+" "
+                            else:
+                                concatenar+=i
+                        texto[indice] = concatenar
+                        j = open("Practica1/archivosOp/solicitudes.txt", "w")
+                        print(texto)
+                        for i in texto:
+                            if i is not texto[-1]:
+                                j.write(f"{i}\n")
+                            else:
+                                j.write(f"{i}") 
+                        j.close()
+                        
+                        # Actualizar inventario general
+                        t = open("Practica1/archivosSistema/inventarioInicial.txt" , "r+")
+                        t.read()
+                        t.write("\n")
+                        inventario = texto[indice].split(" ")
+                        inventario.pop(-1)
+                        inventario.pop(-1)
+                        for i in inventario:
+                            if i == inventario[-1]:
+                                t.write(i)
+                            else: 
+                                t.write(f"{i} ")
+                    else: 
+                        print("índice no válido")
+
+            elif indice==7: # Salir del bucle
+                break
+            else: 
+                print("Indice no válido")
         # ------------------------------------------
     def accesoInvestigador(self, cedula):
         empleado = self.busqueda(cedula,"empleado-ID")
